271add2fbaee5384f292c6e167ead969eee6086e
code-11 <brendan.ritter@yahoo.com>
2015-11-03 11:53:10 -0500

    Started on itenues scraper

--- itunes_scraper/find_sarcasm.py
+++ itunes_scraper/find_sarcasm.py
@@ -16,8 +16,29 @@ Github taken from:
 import itunes
 import AppStoreReviews as app_look
 import pprint
+import csv
 
 UNITED_STATES=143441
+
+SARCASTIC_APP_NUMS=[
+	834335592,
+	336536208,
+	929779844,
+	423942748,
+	788438899,
+	376919110,
+	297048779,
+	522273825,
+	907037398,
+	564226056,
+	321586038,
+	294785375,
+	317367752,
+	284963359,
+	547160634,
+	346204860
+]
+
 
 def get_reviews(result):
 	the_id=result.get_id()
@@ -25,9 +46,28 @@ def get_reviews(result):
 	return reviews 
 
 pp = pprint.PrettyPrinter(indent=4)
-apps = itunes.search(query='strategy games', media='software')
-app=apps[0]
-pp.pprint(get_reviews(app))
+
+
+csvfile=open('sarcastic_reviews.csv', 'wb')
+fieldnames = ['id','rank', 'review','topic','user','version']
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writeheader()
+
+for app_num in SARCASTIC_APP_NUMS:
+	reviews=app_look.getReviews(UNITED_STATES,app_num,maxReviews=-1)
+	for review in reviews:
+		review["id"]=app_num
+		writer.writerow(dict((k, v.encode('utf-8') if type(v) is unicode else v) for k, v in review.iteritems()))
+	print(app_num)
+
+
+# apps = itunes.search(query='strategy games', media='software')
+# app=apps[0]
+# pp.pprint(get_reviews(app))
+
+# reviews=app_look.getReviews(UNITED_STATES,834335592,maxReviews=1)
+# pp.pprint(reviews)
+
 # print(dir(itunes))
 # the_id=app.get_id()
 # print(app_look.getReviews(143441,the_id,maxReviews=100))
